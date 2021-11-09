6a6e38e8541a3b8293023533793362dc82c92ca2
aniruddha-chakravarty <w111heckflosse@gmail.com>
2021-11-09 08:09:44 +0530

    pushing back-end

---
+++ nodejs-express-mongodb/app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: "mongodb://root:root@crud-shard-00-00.3au7w.mongodb.net:27017,crud-shard-00-01.3au7w.mongodb.net:27017,crud-shard-00-02.3au7w.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-38kfii-shard-0&authSource=admin&retryWrites=true&w=majority"
+  };
